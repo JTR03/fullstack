edc5fc9ff0692fbcfcc2ed235cd13b991e2d5419
Jonathan <jonathanrose606@gmail.com>
2023-01-12 22:53:25 +0200

    all filed modified

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,54 +1,26 @@
 const App = () => {
   const course = {
-    name:"Half Stack application development",
+    name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
-        exercises:10,
-       },
-       {
-        name:"Using props to pass data",
-        exercises:7,},
-        {
-          name: "State of a component",
-          exercises:14
-         }
-    ]
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
- 
- 
- 
- 
- 
- 
- 
-  
-  
-  
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course.name} />
-      <Content  part={course.parts}/>
+      <Content part={course.parts} />
       <Total part={course.parts} />
     </div>
   );
@@ -61,17 +33,24 @@ const Header = (props) => {
 };
 
 const Content = (props) => (
-<>
-<Part part = {props.part[0].name} num = {props.part[0].exercises} />
-<Part part = {props.part[1].name} num = {props.part[1].exercises} />
-<Part part =  {props.part[2].name} num = {props.part[2].exercises} />
-</>
- 
- 
+  <>
+    <Part part={props.part[0].name} num={props.part[0].exercises} />
+    <Part part={props.part[1].name} num={props.part[1].exercises} />
+    <Part part={props.part[2].name} num={props.part[2].exercises} />
+  </>
 );
 
-const Total = (props) => <p>Number of exercises {props.part[0].exercises +props.part[1].exercises+props.part[2].exercises}</p>;
+const Total = (props) => (
+  <p>
+    Number of exercises{" "}
+    {props.part[0].exercises +
+      props.part[1].exercises +
+      props.part[2].exercises}
+  </p>
+);
 
-const Part =  (props) => (
-  <p>{props.part} {props.num}</p>
-)
+const Part = (props) => (
+  <p>
+    {props.part} {props.num}
+  </p>
+);
