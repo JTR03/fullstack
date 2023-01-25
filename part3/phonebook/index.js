58f26c6cc5ba066d04e03ef1767cbd66a5783561
Jonathan <jonathanrose606@gmail.com>
2023-01-25 18:58:35 +0200

    implemented morgan logger

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 
+
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
+morgan.token('sent',(req,res) => JSON.stringify(req.body))
 
 let phonebook = [
   {
