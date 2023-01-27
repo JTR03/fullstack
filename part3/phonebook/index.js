81a85963ea53f57e2d68a53a22ac366a0b97353c
Jonathan <jonathanrose606@gmail.com>
2023-01-27 09:14:04 +0200

    optimize for production

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
 morgan.token('sent',(req,res) => JSON.stringify(req.body))
 app.use(cors())
+app.use(express.static('build'))
 
 let phonebook = [
   {
