f4ba8a1da34e19f409377ffab3d7e1e1baa7eb51
Jonathan <jonathanrose606@gmail.com>
2023-01-27 08:41:51 +0200

    backend connected to frontend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const morgan = require("morgan")
 const app = express();
 
@@ -6,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :sent'))
 morgan.token('sent',(req,res) => JSON.stringify(req.body))
+app.use(cors())
 
 let phonebook = [
   {
@@ -94,7 +96,7 @@ app.post("/api/people", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
