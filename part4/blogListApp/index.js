2393d4c118c5374365c8ec1cabf49f27526bbe16
Jonathan <jonathanrose606@gmail.com>
2023-02-07 16:51:54 +0200

    file rarraged according to nodes best practices

--- part4/blogListApp/index.js
+++ part4/blogListApp/index.js
@@ -1,60 +1,47 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./util/config')
+const logger = require('./util/logger')
 
-app.use(cors())
-app.use(express.json())
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+app.listen(config.PORT, () => {
+  logger.info(`Server running on ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://jtr03:LGmsdYnYv5vCcG@cluster0.1ag70ar.mongodb.net/blogs?retryWrites=true&w=majority'
-mongoose.set('strictQuery',false)
-mongoose.connect(mongoUrl)
-
-
-
-
-app.get('/',(req,res) => {
-  res.send('<h1>Welcome To My Fav BlogLists</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    mongoose.connection.close()
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body;
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.json(result)
-    })
-    console.log(body);
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// app.use(cors())
+// app.use(express.json())
+// 
+// 
+// 
+// 
+// app.get('/',(req,res) => {
+  // res.send('<h1>Welcome To My Fav BlogLists</h1>')
+// })
+// 
+// app.get('/api/blogs', (request, response) => {
+  // Blog
+    // .find({})
+    // .then(blogs => {
+      // response.json(blogs)
+    // })
+    // mongoose.connection.close()
+// })
+// 
+// app.post('/api/blogs', (request, response) => {
+  // const body = request.body;
+// 
+  // const blog = new Blog({
+    // title: body.title,
+    // author: body.author,
+    // url: body.url,
+    // likes: body.likes
+  // })
+// 
+  // blog
+    // .save()
+    // .then(result => {
+      // response.json(result)
+    // })
+    // console.log(body);
+// })
+// 
---
+++ part4/blogListApp/util/logger.js
@@ -0,0 +1,11 @@
+const info = (...info) => {
+    console.log(...info)
+}
+
+const error = (...err) => {
+    console.error(...err);
+}
+
+module.exports = {
+    info,error
+}
